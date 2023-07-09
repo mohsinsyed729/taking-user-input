88703bc0f098b89acc8b44edb8cf84594eebb821
mohsinsyed729 <136225413+mohsinsyed729@users.noreply.github.com>
2023-07-09 21:16:16 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,29 @@
+# use of input
+a=input("enter your name:")
+print("my name is", a)
+
+b=input("enter your age:")
+print("my age is",b)
+
+c=input("enter your course name:")
+print("I am persuing", c)
+
+
+x=input("enter first number:")
+y=input("enter second number:")
+#we have to use intor float for both to add them
+print(int(x) +int(y))
+
+#if we don't use int we will not get the sum 
+print(x + y)
+
+#for subtraction
+print (int(x)-int(y))  #without int cannot be used
+
+#for multiplication
+
+print(int(x)*int(y))
+
+#for division
+
+print(int(x)/int(y))
